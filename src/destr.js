27808f6e3107a0ce7c255fa164569d880ce8d753
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-02-19 06:04:04 +0300

    dsa

--- src/destr.js
+++ src/destr.js
@@ -40,3 +40,25 @@ function consume (numberOfGallons) {
 }
 
 consume(3);
+
+let counter = 1;
+
+function addDivToHeader () {
+  const newDiv = document.createElement('div');
+  newDiv.textContent = 'div number ' + counter;
+
+  counter = counter + 1;
+
+  const headerSection = document.querySelector('header');
+  headerSection.appendChild(newDiv)
+}
+
+function addDivToFooter() {
+  const newDiv = document.createElement('div');
+  newDiv.textContent = 'div number ' + counter;
+
+  counter = counter + 1;
+
+  const headerSection = document.querySelector('footer');
+  headerSection.appendChild(newDiv)
+}
