be4c1ba8f24245beefd7ead43f99a481bafec935
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-02-22 12:09:27 +0300

    add dsa

---
+++ src/turtle.js
@@ -0,0 +1,11 @@
+def spiral(sides):
+    for n in range(sides):
+        t.forward(n)
+        t.right(30)
+
+spiral(50)
+// equivalent code in JavaScript
+var count = 1; 
+var name = "Julia";
+var num = 1.2932;
+var price = 2.99;
--- src/turtle.py
+++
@@ -1,6 +0,0 @@
-def spiral(sides):
-    for n in range(sides):
-        t.forward(n)
-        t.right(30)
-
-spiral(50)
