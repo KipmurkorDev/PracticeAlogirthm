d2c8b9da900926e5e6c36e1b059d6c9c183fae64
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-02-23 11:57:34 +0300

    add dsa

--- src/httpExtenalAPI.js
+++ src/httpExtenalAPI.js
@@ -114,3 +114,22 @@ const requestListener2 = function (req, res) {
   res.writeHead(200);
   res.end(`{"message": "This is a JSON response"}`);
 };
+
+// content of index.js
+const http = require('http')
+const port2 = 3000
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port2, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
