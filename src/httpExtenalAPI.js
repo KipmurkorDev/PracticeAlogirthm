e07a78d9325471e0b0813cf28d35c45895028859
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-02-23 11:58:47 +0300

    add dsa

--- src/httpExtenalAPI.js
+++ src/httpExtenalAPI.js
@@ -149,3 +149,19 @@ app.listen(port4, (err) => {
 
   console.log(`server is listening on ${port}`)
 })
+
+
+// index.js
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app3 = express()
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
