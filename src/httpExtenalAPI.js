08c7967900e0a1416b939cc024e4069a51176bf2
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-02-23 11:56:07 +0300

    add dsa

--- src/httpExtenalAPI.js
+++ src/httpExtenalAPI.js
@@ -95,3 +95,15 @@ const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
+// 
+const http = require("http");
+
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
