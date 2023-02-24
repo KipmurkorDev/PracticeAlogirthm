2a57b7bb395139246f3b02739d344575b15c7e3d
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-02-24 11:34:53 +0300

    dsa add

--- src/fileServer.js
+++ src/fileServer.js
@@ -18,22 +18,22 @@ console.log('Listening on :8080')
 // 
 var express = require('express'),
     htmlDir = './html/'
-var app3 = express();
+    var app3 = express();
 
-//Log all requests
-app.use(express.logger());
-
-//Set content directories
-app.use(express.static(__dirname + '/html'));
-app.use('/js',express.static(__dirname + '/js'));
-app.use('/css', express.static(__dirname + '/css'));
-app.use("/image", express.static(__dirname + '/image'));
-
-app.get('/', function(request, response) {
-    response.sendfile(htmlDir + 'turbocalendulator.html');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+    //Log all requests
+    app.use(express.logger());
+    
+    //Set content directories
+    app.use(express.static(__dirname + '/html'));
+    app.use('/js',express.static(__dirname + '/js'));
+    app.use('/css', express.static(__dirname + '/css'));
+    app.use("/image", express.static(__dirname + '/image'));
+    
+    app.get('/', function(request, response) {
+        response.sendfile(htmlDir + 'turbocalendulator.html');
+    });
+    
+    var port = process.env.PORT || 5000;
+    app.listen(port, function() {
+      console.log("Listening on " + port);
+    });
