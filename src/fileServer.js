bb9450c90d573a62884db2da272983212be6e2c1
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-02-24 11:36:22 +0300

    dsa add

--- src/fileServer.js
+++ src/fileServer.js
@@ -51,3 +51,14 @@ async function example() {
   }
 }
 example();
+const fs = require('fs/promises');
+
+async function example() {
+  try {
+    const data = await fs.readFile('/Users/joe/test.txt', { encoding: 'utf8' });
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+example();
