8e6a13d1c7d9ff029fbc20342a1ff9b8a800c4fc
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-02-24 11:36:56 +0300

    dsa add

--- src/fileServer.js
+++ src/fileServer.js
@@ -62,3 +62,15 @@ async function example() {
   }
 }
 example();
+
+
+const fs = require('fs');
+
+const content = 'Some content!';
+
+fs.writeFile('/Users/joe/test.txt', content, err => {
+  if (err) {
+    console.error(err);
+  }
+  // file written successfully
+});
