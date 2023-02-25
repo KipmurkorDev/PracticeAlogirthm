276bf35444cad8ec8a5f74f227b40392f742c7df
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-02-25 07:10:58 +0300

    add dsa

--- src/nodejs.js
+++ src/nodejs.js
@@ -10,3 +10,16 @@ process.stdin.on('readable', function() {
 process.stdin.on('end', function() {
   process.stdout.write('end');
 });
+
+
+if (process.getgid && process.setgid) {
+    console.log('Current gid: ' + process.getgid());
+    try {
+      process.setgid(501);
+      console.log('New gid: ' + process.getgid());
+    }
+    catch (err) {
+      console.log('Failed to set gid: ' + err);
+    }
+  }
+  
