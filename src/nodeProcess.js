ac06f0a0b10b7bf701c8f7a55c464116849e8d36
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-02-27 11:47:18 +0300

    process node

--- src/nodeProcess.js
+++ src/nodeProcess.js
@@ -8,3 +8,20 @@ const fs = require('fs-extra')
 
 fs.copy('/tmp/myfile', '/tmp/mynewfile')
   .then(() => console.log('success!'))
+
+
+
+  // Write Javascript code here
+var cp = require('child_process');
+
+var child = cp.fork(__dirname + '/sub.js');
+
+child.on('message', function(m) {
+console.log('Parent process received:', m);
+});
+
+child.send({ hello: 'from parent process' });
+
+child.on('close', (code) => {
+console.log(`child process exited with code ${code}`);
+});
