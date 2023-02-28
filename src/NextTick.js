bd5f97d73f88a6287cf5203f086fd749068625b1
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-02-28 15:24:59 +0300

    dsa add

---
+++ src/NextTick.js
@@ -0,0 +1,26 @@
+console.log("Hello => number 1");
+
+setTimeout(() => {
+  console.log("The timeout running last => number 4");
+}, 0);
+
+setImmediate(() => {
+  console.log("Running before the timeout => number 3");
+});
+
+process.nextTick(() => {
+  console.log("Running at next tick => number 2");
+});
+
+// 
+// Including the module into out project
+var process = require('process');
+
+// It will return the current working directory
+console.log('this is the working directory --> ' + process.cwd());
+
+// It will return the version of process we are using
+console.log('this is the process version --> ' + process.version);
+
+// It will return the type of OS we are using at that time.
+console.log('current OS we are using --> ' + process.platform);
