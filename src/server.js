ad389e9341f7d7d0e104daf608464d726bacc915
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-02 10:00:00 +0300

    add dsa

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const requestListener = function (req, res) { 
+    res.writeHead(200); 
+    res.end("Hello from Server!"); 
+   }; 
+   
+   const server = http.createServer(requestListener); 
+server.listen(port, host, () => { 
+ console.log(`Server is running on http://${host}:${port}`); 
+}); 
