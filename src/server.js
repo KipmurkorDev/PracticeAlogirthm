cb763bd38866b62279294a239521f120f2209438
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-02 10:00:29 +0300

    add dsa

--- src/server.js
+++ src/server.js
@@ -7,3 +7,18 @@ const requestListener = function (req, res) {
 server.listen(port, host, () => { 
  console.log(`Server is running on http://${host}:${port}`); 
 }); 
+
+
+const requestListenerw = function (req, res) { 
+    fs.readFile(__dirname + "/samplePage.html") 
+    .then(contents => { 
+    res.setHeader("Content-Type", "text/html"); 
+    res.writeHead(200); 
+    res.end(contents); 
+    }) 
+   .catch(err => { 
+    res.writeHead(500); 
+    res.end(err); 
+    return; 
+    }); 
+   }; 
