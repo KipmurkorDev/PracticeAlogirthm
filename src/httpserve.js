b262da33db21d096bf5beb069a9d5fbd929c00d9
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-03 13:10:36 +0300

    add dsa

---
+++ src/httpserve.js
@@ -0,0 +1,17 @@
+const request = https.request(url, (response) => {
+    let data = '';
+    response.on('data', (chunk) => {
+        data = data + chunk.toString();
+    });
+  
+    response.on('end', () => {
+        const body = JSON.parse(data);
+        console.log(body);
+    });
+})
+  
+request.on('error', (error) => {
+    console.log('An error', error);
+});
+  
+request.end() 
