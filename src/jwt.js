561728cc29a4f5db99923d2e28641b21790cc7bc
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-05 11:10:02 +0300

    dsa add

--- src/jwt.js
+++ src/jwt.js
@@ -9,3 +9,13 @@ function signJWT(data) {
 module.exports = {
     signJWT
 }
+const Supabase = require('@supabase/supabase-js');
+
+const client = Supabase.createClient(process.env.SUPA_URL, process.env.SUPA_KEY);
+
+const authClient = Supabase.createClient(process.env.SUPA_URL, process.env.SUPA_KEY);
+
+module.exports = {
+    client,
+    authClient
+};
