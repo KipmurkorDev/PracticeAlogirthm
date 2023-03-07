f15967fa69ebab1a7847133fd4eda976173834a1
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-07 12:28:19 +0300

    add dsa

--- src/nodej.js
+++ src/nodej.js
@@ -23,3 +23,15 @@ function isAdult(user) {
   };
   
   const isJustineAnAdult= isAdult(justine, "I shouldn't be here!");
+
+
+  // Assume add.wasm file exists that contains a single function adding 2 provided arguments
+const fs = require('fs');
+
+const wasmBuffer = fs.readFileSync('/path/to/add.wasm');
+WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
+  // Exported function live under instance.exports
+  const { add } = wasmModule.instance.exports;
+  const sum = add(5, 6);
+  console.log(sum); // Outputs: 11
+});
