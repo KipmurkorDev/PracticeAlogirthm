15cbe45ce3843fd44b6f953ddf7cc0d690dfe9a1
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-11 10:49:04 +0300

    dsa add

--- src/microService.js
+++ src/microService.js
@@ -28,4 +28,48 @@ app.get('/tweet', async (req, res) => {
       res.send(500);
     }
   });
+  app.get('/message', async (req, res) => {
+    const response = await axios.get('http://localhost:8001/');
+  
+    try {
+      console.log(response.data);
+  
+      res.send(response.data);
+    } catch (error) {
+      console.log(error);
+  
+      res.send(500);
+    }
+  });
+  
+  let twitterEmitter = new Twitter();
+
+  // Creating the event listener and callback function
+
+  // postTweet is the name of the event
+
+  twitterEmitter.on(
+    'postTweet',
+
+    (id, tweet, comments, retweets, likes, date) => {
+      const data = {
+        id: id,
+
+        tweet: tweet,
+
+        comments: comments,
+
+        retweets: retweets,
+
+        likes: likes,
+
+        date: date,
+      };
+
+      console.log(data);
+
+      res.json(data);
+    }
+  );
+
   
