f1ad7984eb1f825d5558b756b17ad46bc0991e52
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-11 13:26:01 +0300

    dsa add

--- src/path.js
+++ src/path.js
@@ -4,3 +4,15 @@ const path = require('path')
 console.log(path.resolve('ayyay.js')); // '/Users/joe/joe.txt' if run from my home folder
 
 // path join, resolve and normalized()
+
+
+const fs = require('fs');
+
+const folderPath = '/Users/kipmurkoremmanuel/Desktop'
+
+const folerPaths=fs.readdirSync(folderPath).map(fileName => {
+    return path.join(folderPath, fileName);
+  });
+
+
+  console.log(folerPaths);
