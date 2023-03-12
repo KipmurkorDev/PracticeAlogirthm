3cee6e5f0ca1f9544bd30cd17b4904fb6470eac9
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-12 12:16:26 +0300

    add dsa

--- fuct.sql
+++ fuct.sql
@@ -1,4 +1,4 @@
-Create or Replace function add_user(id int,firstname text,lastname text,user_email varchar, access_token text, status boolean, job text, isStudent boolean,inactivity boolean, deleted boolean) 
+Create or Replace function add_user(id int,firstname varchar,lastname varchar,user_email varchar, access_token varchar, status varchar, job varchar, isstudent boolean,inactivity int, deleted boolean) 
 returns text
 language plpgsql
 AS 
@@ -10,7 +10,7 @@ select count(*) into user_exist from public.users where email = user_email;
 IF(user_exist=0)
 THEN
   INSERT INTO public.users (id,"firstName","lastName",email,access_token, status, job, "isStudent",inactivity, deleted) 
-  VALUES(id,firstName,lastName,user_email, access_token, status, job, isStudent,inactivity, deleted );
+  VALUES(id,firstname,lastname,user_email, access_token, status, job, isstudent,inactivity, deleted );
   return 'User added successffully';
 ELSE
   return 'User Already exist';
