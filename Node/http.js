0c823f1f52f817f4ca9fa84119ab2ae0b5081aca
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-13 12:00:09 +0300

    dsa add

---
+++ Node/http.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(8080);
