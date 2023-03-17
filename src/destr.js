c1eb42e2f3afdb28da1b99df8f251d1c2abba546
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-17 07:16:53 +0300

    das ada

--- src/destr.js
+++ src/destr.js
@@ -69,3 +69,39 @@ function Mouse() {
   }
   
   Mouse.prototype = rodent;
+
+
+  import mongoose from "mongoose"
+import { Pagination } from "./Pagination";
+
+// type FilterType = 'equal' | 'like' | 'gt' | 'lt'
+
+// export type Filter = {
+//     key: string,
+//     type: FilterType,
+//     value: string | number
+// }
+
+
+
+
+
+export function createAggregation(prj_id, collection_id,  pagination, filters) {
+
+    const collectionObject = new mongoose.Types.ObjectId(collection_id);
+
+    const result = [
+        { $match: { prj_id: parseInt(prj_id.toString()), collection_id: collectionObject } },
+        { $skip: pagination.page * pagination.pageElems },
+        { $limit: pagination.pageElems },
+    ]
+
+    for (const filter of filters) {
+        const fn = FilterFunctionMapping[filter.type];
+        const res = { $match: fn(filter.key, filter.value) }
+        result.push(res);
+    }
+
+    return result;
+
+}
