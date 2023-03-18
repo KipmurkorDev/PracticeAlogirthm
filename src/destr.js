bada721af22af7508053597c9f9bf8964676a706
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-18 08:06:00 +0300

    dsa add

--- src/destr.js
+++ src/destr.js
@@ -105,3 +105,22 @@ export function createAggregation(prj_id, collection_id,  pagination, filters) {
     return result;
 
 }
+function Bird(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 2;
+}
+let swan = new Bird();
+swan.name = "Carlos";
+swan.color = "white";
+
+
+let ownProps = [];
+
+for (let property in duck) {
+if(duck.hasOwnProperty(property)) {
+  ownProps.push(property);
+}
+}
+
+console.log(ownProps);
