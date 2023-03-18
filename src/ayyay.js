99c42075e9a9b594c4a3952e765bf1bfcf50d5fb
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-18 08:04:25 +0300

    dsa add

--- src/ayyay.js
+++ src/ayyay.js
@@ -195,3 +195,22 @@ eventEmitter.on('start', () => {
   });
   eventEmitter.emit('start');
 
+  function Bird(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 2;
+  }
+  let swan = new Bird();
+swan.name = "Carlos";
+swan.color = "white";
+
+
+let ownProps = [];
+
+for (let property in duck) {
+  if(duck.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}
+
+console.log(ownProps);
