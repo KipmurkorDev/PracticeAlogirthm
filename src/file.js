12e8a0b1f55e4240785bec4e794cf62465517517
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-19 07:01:49 +0300

    add dsa

--- src/file.js
+++ src/file.js
@@ -16,3 +16,24 @@ function pluck(list, propertyName) {
       return i[propertyName];
     });
   }
+
+const jwt = require('jsonwebtoken');
+
+function signJWT(data) {
+    return jwt.sign(data, process.env.JWT_KEY, { expiresIn: '7d' });
+}
+
+
+module.exports = {
+    signJWT
+}
+const Supabase = require('@supabase/supabase-js');
+
+const client = Supabase.createClient(process.env.SUPA_URL, process.env.SUPA_KEY);
+
+const authClient = Supabase.createClient(process.env.SUPA_URL, process.env.SUPA_KEY);
+
+module.exports = {
+    client,
+    authClient
+};
