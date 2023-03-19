7c634ded93a95784730c81964b7d23ee926e36d0
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-19 07:01:29 +0300

    add dsa

--- src/file.js
+++ src/file.js
@@ -3,3 +3,16 @@ fs.readFile("/file.md", (err, data) => {
   });
   
   myAwesomeFunction();
+  var employees=[]
+employees[0]={name:"George", age:32, retiredate:"March 12, 2014"}
+employees[1]={name:"Edward", age:17, retiredate:"June 2, 2023"}
+employees[2]={name:"Christine", age:58, retiredate:"December 20, 2036"}
+employees[3]={name:"Sarah", age:62, retiredate:"April 30, 2020"}
+
+
+
+function pluck(list, propertyName) {
+    return list.map(function(i) {
+      return i[propertyName];
+    });
+  }
