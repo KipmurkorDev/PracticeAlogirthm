83436564f3f095b324672c59a30c75c7e90bbf12
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-19 07:01:06 +0300

    add dsa

--- src/obj.js
+++ src/obj.js
@@ -18,3 +18,48 @@ const obj={
     },
     "homepage": "https://github.com/Link-/PR-metadata-action#readme"
   }
+  const duck = {
+    hasBill: true
+  };
+  const beaver = {
+    hasTail: true
+  };
+  const otter = {
+    hasFur: true,
+    feet: 'webbed'
+  };
+  
+  function Basketball(color) {
+    return {
+      color: color,
+      numDots: 35000
+    };
+  }
+  function CoffeeMaker(object) {
+    let needsRefill = false;
+  
+    return Object.assign({}, object, {
+      pourAll: function () {
+        needsRefill = true;
+      },
+      isEmpty: function () {
+        return needsRefill;
+      }
+    });
+  }
+
+ const newobtc= {
+    style: 'percolator',
+    pourAll: function () {
+      needsRefill = true;
+    },
+    isEmpty: function () {
+      return needsRefill;
+    }
+  }
+  let developer = {
+    name: 'Veronika',
+    getName: function () {
+      return this.name;
+    }
+  };
