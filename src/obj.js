6001de4b434281165b823e039c59a04dbdfc71f8
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-20 07:20:20 +0300

    add dsa

--- src/obj.js
+++ src/obj.js
@@ -68,3 +68,36 @@ while (x >= 1) {
   console.log("hello " + x);
   x = x - 1;
 }
+
+const path = require('path')
+
+console.log(path.resolve('ayyay.js')); // '/Users/joe/joe.txt' if run from my home folder
+
+// path join, resolve and normalized()
+
+
+const fs = require('fs');
+const isFile = fileName => {
+    return fs.lstatSync(fileName).isFile();
+  };
+  
+const folderPath = '/Users/kipmurkoremmanuel/Desktop'
+
+const folderPaths=fs.readdirSync(folderPath).map(fileName => {
+    return path.join(folderPath, fileName);
+  }).filter(isFile)
+
+//  remove folder
+const fs = require('fs');
+
+fs.rmdir(dir, err => {
+  if (err) {
+    throw err;
+  }
+
+  console.log(`${dir} is deleted!`);
+});
+
+  
+
+  console.log(folderPaths);
