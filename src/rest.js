15078a0ef46657fca5a081b3516026aebc7eddeb
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-21 07:52:32 +0300

    dsa add

--- src/rest.js
+++ src/rest.js
@@ -84,3 +84,27 @@ describe('Bitbucket Provider',() => {
     });
 });
 
+const requestListener = function (req, res) { 
+    res.writeHead(200); 
+    res.end("Hello from Server!"); 
+   }; 
+   
+   const server = http.createServer(requestListener); 
+server.listen(port, host, () => { 
+ console.log(`Server is running on http://${host}:${port}`); 
+}); 
+
+
+const requestListenerw = function (req, res) { 
+    fs.readFile(__dirname + "/samplePage.html") 
+    .then(contents => { 
+    res.setHeader("Content-Type", "text/html"); 
+    res.writeHead(200); 
+    res.end(contents); 
+    }) 
+   .catch(err => { 
+    res.writeHead(500); 
+    res.end(err); 
+    return; 
+    }); 
+   }; 
