69633f08b01207dc9756418e9f901c8ac48b746d
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-21 08:01:15 +0300

    dsa add

--- src/readline.js
+++ src/readline.js
@@ -21,3 +21,27 @@ const questions = [
 inquirer.prompt(questions).then(answers => {
   console.log(`Hi ${answers.name}!`);
 });
+const requestListener = function (req, res) { 
+  res.writeHead(200); 
+  res.end("Hello from Server!"); 
+ }; 
+ 
+ const server = http.createServer(requestListener); 
+server.listen(port, host, () => { 
+console.log(`Server is running on http://${host}:${port}`); 
+}); 
+
+
+const requestListenerw = function (req, res) { 
+  fs.readFile(__dirname + "/samplePage.html") 
+  .then(contents => { 
+  res.setHeader("Content-Type", "text/html"); 
+  res.writeHead(200); 
+  res.end(contents); 
+  }) 
+ .catch(err => { 
+  res.writeHead(500); 
+  res.end(err); 
+  return; 
+  }); 
+ }; 
