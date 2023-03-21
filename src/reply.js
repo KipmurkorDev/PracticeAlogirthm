518ce099847eb761b691b11bc42d1fcf4547097f
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-21 07:52:13 +0300

    dsa add

--- src/reply.js
+++ src/reply.js
@@ -10,3 +10,13 @@ for (let i = 1; i <= 12; i++) {
 }
 
 console.log(solution);
+function calc() {
+  var extra = 5;
+  var sum = function(a,b) {
+    return a + b;
+  }
+  let total = extra + sum(2,3);
+}
+console.log(extra); // is extra accessible here?
+calc();
+console.log(total); // is total accessible here?
