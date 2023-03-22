cda2f6a602e08b9f8b9094732d6e468b43b978b7
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-22 08:18:22 +0300

    add dsa

--- src/destr.js
+++ src/destr.js
@@ -124,3 +124,14 @@ if(duck.hasOwnProperty(property)) {
 }
 
 console.log(ownProps);
+// A collection of the values 1, 2 and 3
+const arr = [1, 2, 3]
+
+// Each value is related to one another, in the sense that each is indexed in a position of the array
+const indexOfTwo = arr.indexOf(2)
+console.log(arr[indexOfTwo-1]) // 1
+console.log(arr[indexOfTwo+1]) // 3
+
+// We can perform many operations on the array, like pushing new values into it
+arr.push(4)
+console.log(arr) // [1,2,3,4]
