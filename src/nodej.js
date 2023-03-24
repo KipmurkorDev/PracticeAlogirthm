05ffbd6a94f89704d4bbbecfbf8aaa875e0ef353
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-24 08:40:17 +0300

    dsa add

--- src/nodej.js
+++ src/nodej.js
@@ -35,3 +35,24 @@ WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
   const sum = add(5, 6);
   console.log(sum); // Outputs: 11
 });
+
+const jwt = require('jsonwebtoken');
+
+function signJWT(data) {
+    return jwt.sign(data, process.env.JWT_KEY, { expiresIn: '7d' });
+}
+
+
+module.exports = {
+    signJWT
+}
+const Supabase = require('@supabase/supabase-js');
+
+const client = Supabase.createClient(process.env.SUPA_URL, process.env.SUPA_KEY);
+
+const authClient = Supabase.createClient(process.env.SUPA_URL, process.env.SUPA_KEY);
+
+module.exports = {
+    client,
+    authClient
+};
