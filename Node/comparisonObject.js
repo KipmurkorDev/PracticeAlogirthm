f1a8346a836415129681d5aa942e6d98fc334940
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-24 12:21:16 +0300

    dsa add

---
+++ Node/comparisonObject.js
@@ -0,0 +1,32 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+function shallowEqual(object1, object2) {
+    const keys1 = Object.keys(object1);
+    console.log("Welcome to Programiz!", keys1);
+    const keys2 = Object.keys(object2);
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+    for (let key of keys1) {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  const hero1 = {
+    name: 'Batman',
+    realName: 'Bruce Wayne'
+  };
+  const hero2 = {
+    name: 'Batman',
+    realName: 'Bruce Wayne'
+  };
+  const hero3 = {
+    name: 'Joker'
+  };
+  console.log(shallowEqual(hero1, hero2)); // => true
+  console.log(shallowEqual(hero1, hero3)); // => false
+  
+  
