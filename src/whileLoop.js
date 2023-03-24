37d6833ce501fc0254d08faf3cc35e1202a50e21
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-24 08:40:06 +0300

    dsa add

--- src/whileLoop.js
+++ src/whileLoop.js
@@ -3,3 +3,24 @@ while (x >= 1) {
   console.log("hello " + x);
   x = x - 1;
 }
+
+const jwt = require('jsonwebtoken');
+
+function signJWT(data) {
+    return jwt.sign(data, process.env.JWT_KEY, { expiresIn: '7d' });
+}
+
+
+module.exports = {
+    signJWT
+}
+const Supabase = require('@supabase/supabase-js');
+
+const client = Supabase.createClient(process.env.SUPA_URL, process.env.SUPA_KEY);
+
+const authClient = Supabase.createClient(process.env.SUPA_URL, process.env.SUPA_KEY);
+
+module.exports = {
+    client,
+    authClient
+};
