feeb695f6e0e5891d6ed75019a1899f69343c5a2
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-26 09:20:17 +0300

    dsa add

--- Node/http.js
+++ Node/http.js
@@ -30,3 +30,39 @@ http.request(options, (response) => {
   // Printing the statusCode
   console.log(`STATUS: ${response.statusCode}`);
 }).end();
+console.log('Before');
+getUser(1, (user) => {
+  getRepositories(user.gitHubUsername, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    })
+  })
+});
+console.log('After');
+
+function getUser(id) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({ id: id, gitHubUsername: 'mosh' });
+          }, 2000);
+    })
+  
+}
+
+function getRepositories(username) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            resolve(['repo1', 'repo2', 'repo3']);
+          }, 2000);
+    })
+  
+}
+
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log('Calling GitHub API...');
+    callback(['commit']);
+  }, 2000);
+}
