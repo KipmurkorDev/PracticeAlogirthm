edb4f7f8efd3e1404c9369076e18d27669655628
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-27 12:54:03 +0300

    dsa add

--- Node/promise.js
+++ Node/promise.js
@@ -34,3 +34,71 @@ function getCommits(repo, callback) {
     callback(['commit']);
   }, 2000);
 }
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(8080);
+
+
+
+var options = {
+  host: 'www.geeksforgeeks.org',
+  path: '/courses',
+  method: 'GET'
+};
+
+// Making a get request to 
+// 'www.geeksforgeeks.org'
+http.request(options, (response) => {
+
+  // Printing the statusCode
+  console.log(`STATUS: ${response.statusCode}`);
+}).end();
+console.log('Before');
+getUser(1, (user) => {
+  getRepositories(user.gitHubUsername, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    })
+  })
+});
+console.log('After');
+
+function getUser(id) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({ id: id, gitHubUsername: 'mosh' });
+          }, 2000);
+    })
+  
+}
+
+function getRepositories(username) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            resolve(['repo1', 'repo2', 'repo3']);
+          }, 2000);
+    })
+  
+}
+
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log('Calling GitHub API...');
+    callback(['commit']);
+  }, 2000);
+}
