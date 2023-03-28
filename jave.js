e0b02ab5803c818149a4918dae811eacec12ca56
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-28 09:53:04 +0300

    add dsa

---
+++ jave.js
@@ -0,0 +1,51 @@
+// {
+//     "authData": {
+//       "zapier_api_key": ":censored:128:7479d4e6f8:"
+//     },
+//     "inputData": {},
+//     "inputDataRaw": {},
+//     "meta": {
+//       "isLoadingSample": false,
+//       "isFillingDynamicDropdown": false,
+//       "isTestingAuth": true,
+//       "isPopulatingDedupe": false,
+//       "limit": -1,
+//       "page": 0,
+//       "isBulkRead": false,
+//       "zap": {
+//         "help": [
+//           "This data structure is provided for backwards compatibility,",
+//           "and should not be relied upon in a Zapier integration."
+//         ],
+//         "id": 38630958,
+//         "link": "https://zapier.com/app/editor/38630958",
+//         "live": true,
+//         "name": "A Zap",
+//         "user": {
+//           "timezone": "Africa/Nairobi"
+//         },
+//         "trigger": {
+//           "service": {
+//             "logos": {
+//               "16x16": "https://cdn.zapier.com/storage/photos/73a6433488cca7c5bd6ed9836fd6d9c8.png",
+//               "32x32": "https://cdn.zapier.com/storage/photos/73a6433488cca7c5bd6ed9836fd6d9c8.png",
+//               "64x64": "https://cdn.zapier.com/storage/photos/73a6433488cca7c5bd6ed9836fd6d9c8.png",
+//               "128x128": "https://cdn.zapier.com/storage/photos/73a6433488cca7c5bd6ed9836fd6d9c8.png"
+//             },
+//             "name": "A Zapier App"
+//           }
+//         },
+//         "action": {
+//           "service": {
+//             "logos": {
+//               "16x16": "https://cdn.zapier.com/storage/photos/73a6433488cca7c5bd6ed9836fd6d9c8.png",
+//               "32x32": "https://cdn.zapier.com/storage/photos/73a6433488cca7c5bd6ed9836fd6d9c8.png",
+//               "64x64": "https://cdn.zapier.com/storage/photos/73a6433488cca7c5bd6ed9836fd6d9c8.png",
+//               "128x128": "https://cdn.zapier.com/storage/photos/73a6433488cca7c5bd6ed9836fd6d9c8.png"
+//             },
+//             "name": "A Zapier App"
+//           }
+//         }
+//       }
+//     }
+//   }
