27b2e7dfb7e19f0cba10357202865ffaa61be60d
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-03-31 12:33:04 +0300

    add dsa

--- Node/process.js
+++ Node/process.js
@@ -9,3 +9,14 @@ process.on('exit', (code) => {
 });
 
 console.log('This message is displayed first.');
+const WEEKDAY = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']; 
+export function weekDayFromDate(date) { 
+if (!(date instanceof Date)) { 
+date = new Date(date); 
+} 
+return WEEKDAY[date.getWeekday()]; 
+} 
+// weekday.mjs (ES Module) 
+import { weekDayFromDate } from './weekday-from-date.mjs'; 
+const dateString = process.argv[2] ?? null; 
+console.log(weekDayFromDate(dateString));
