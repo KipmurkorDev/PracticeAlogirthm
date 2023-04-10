415d0196441818a83d9fed9e54d6566aacf52127
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-04-10 07:32:56 +0300

    add dsa

--- Node/promise.js
+++ Node/promise.js
@@ -173,3 +173,24 @@ function addCount() {
   import { weekDayFromDate } from './weekday-from-date.mjs'; 
   const dateString = process.argv[2] ?? null; 
   console.log(weekDayFromDate(dateString));
+
+
+  function getUser(id) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({ id: id, gitHubUsername: 'mosh' });
+          }, 2000);
+    })
+  
+}
+
+function getRepositories(username) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            resolve(['repo1', 'repo2', 'repo3']);
+          }, 2000);
+    })
+  
+}
