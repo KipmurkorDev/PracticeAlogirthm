3a20b5703c420de7f03b69cca98bd561ef114a59
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-04-11 08:26:00 +0300

    add dsa

--- src/path.js
+++ src/path.js
@@ -97,3 +97,51 @@ fs.rmdir(dir, err => {
   module.exports={
       getcredetialMockFuc
   }
+
+  const readline = require('readline')
+const rls=readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  rls.question(`What's your name?`, name => {
+    console.log(`Hi ${name}!`);
+    rls.close();
+  });
+  const inquirer = require('inquirer');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's your name?",
+  },
+];
+
+inquirer.prompt(questions).then(answers => {
+  console.log(`Hi ${answers.name}!`);
+});
+const requestListener = function (req, res) { 
+  res.writeHead(200); 
+  res.end("Hello from Server!"); 
+ }; 
+ 
+ const server = http.createServer(requestListener); 
+server.listen(port, host, () => { 
+console.log(`Server is running on http://${host}:${port}`); 
+}); 
+
+
+const requestListenerw = function (req, res) { 
+  fs.readFile(__dirname + "/samplePage.html") 
+  .then(contents => { 
+  res.setHeader("Content-Type", "text/html"); 
+  res.writeHead(200); 
+  res.end(contents); 
+  }) 
+ .catch(err => { 
+  res.writeHead(500); 
+  res.end(err); 
+  return; 
+  }); 
+ }; 
