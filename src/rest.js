136653ab2cfb6f2e1c93e0fbfa59b775c63e4ec0
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-04-12 18:23:50 +0300

    add dsa

--- src/rest.js
+++ src/rest.js
@@ -108,3 +108,26 @@ const requestListenerw = function (req, res) {
     return; 
     }); 
    }; 
+
+   var x = 9;
+while (x >= 1) {
+  console.log("hello " + x);
+  x = x - 1;
+}
+let solution = 1;
+
+for (let i = 1; i <= 12; i++) {
+  solution *= i;
+}
+
+console.log(solution);
+function calc() {
+  var extra = 5;
+  var sum = function(a,b) {
+    return a + b;
+  }
+  let total = extra + sum(2,3);
+}
+console.log(extra); // is extra accessible here?
+calc();
+console.log(total); // is total accessible here?
