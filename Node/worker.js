b76a6ac610ae5644ee93157d19d52ffbb08903af
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-04-16 14:32:24 +0300

    add dsa

--- Node/worker.js
+++ Node/worker.js
@@ -75,3 +75,21 @@ const Homepage = () => {
       </>
     );
   };
+
+  self.onmessage = function (e) {
+    const workerData = e.data;
+    postMessage("[WORKER] Web worker onmessage established");
+    switch (workerData.connectionStatus) {
+      case "init":
+        socketInstance = createSocketInstance();
+        socketManagement();
+        break;
+  
+      case "stop":
+        socketInstance.close();
+        break;
+  
+      default:
+        socketManagement();
+    }
+  }
