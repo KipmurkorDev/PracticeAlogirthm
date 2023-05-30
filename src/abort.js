741e06c95c50804c2a7f9adb62fc8b4fb90f8306
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2023-05-30 09:21:39 +0300

    add more

--- src/abort.js
+++ src/abort.js
@@ -9,22 +9,3 @@ console.log(ac.signal.aborted);  // Prints Tr
 
 
 console.log("helllo");
-Create or Replace function add_user(firstname varchar,lastname varchar,user_email varchar, access_token varchar, status varchar, job varchar, isstudent boolean,inactivity int, deleted boolean) 
-returns text
-language plpgsql
-AS 
-$$
-DECLARE 
-user_exist int;
-BEGIN 
-select count(*) into user_exist from public.users where email = user_email;
-IF(user_exist=0)
-THEN
-  INSERT INTO public.users ("firstName","lastName",email,access_token, status, job, "isStudent",inactivity, deleted) 
-  VALUES(firstname,lastname,user_email, access_token, status, job, isstudent,inactivity, deleted );
-  return 'User added successffully';
-ELSE
-  return 'User Already exist';
-END IF;
-END
-$$ 
