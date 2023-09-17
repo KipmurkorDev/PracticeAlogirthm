d74d380dfbf54ba105241476727e8e86c9af30c7
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2023-09-17 10:42:42 +0300

    .

--- HackerRank/repeatedString.js
+++ HackerRank/repeatedString.js
@@ -1,19 +1,21 @@
 // There is a string, , of lowercase English letters that is repeated infinitely many times. Given an integer, , find and print the number of letter a's in the first  letters of the infinite string.
 
 function repeatedString(s, n) {
-  // Write your code here
   let count = 0;
-  let str = "";
-  if (s === "a") {
-    return n;
-  } else if (s.length < n) {
-    let multiplier = Math.ceil(n / s.length);
-    str = s.repeat(multiplier);
-  }
-
-  for (let i = 0; i < n; i++) {
-    if (str[i] === "a") {
+  if (s === "a") return n;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "a") {
       count++;
+    }
+  }
+  const fullRepeats = Math.floor(n / s.length);
+  count *= fullRepeats;
+  const remainingChars = n % s.length;
+  if (remainingChars !== 0) {
+    for (let i = 0; i < remainingChars; i++) {
+      if (s[i] === "a") {
+        count++;
+      }
     }
   }
   return count;
