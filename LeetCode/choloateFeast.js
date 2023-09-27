7cb47da5696e17a73fb1cf5bc7779e1bc9b9c77d
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2023-09-27 05:46:33 +0300

    .

---
+++ LeetCode/choloateFeast.js
@@ -0,0 +1,11 @@
+function chocolateFeast(n, c, m) {
+  // Write your code here
+  let wrappers = Math.floor(n / c);
+  let totalCholate = Math.floor(n / c);
+  while (wrappers >= m) {
+    wrappers -= m;
+    totalCholate++;
+    wrappers++;
+  }
+  return totalCholate;
+}
