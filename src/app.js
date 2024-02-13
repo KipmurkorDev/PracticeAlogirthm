2cac487a42087630945f24f1bbb6533eef2e2470
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-02-13 20:51:10 +0300

    add form

--- src/app.js
+++ src/app.js
@@ -55,3 +55,18 @@ function chocolateFeast(n, c, m) {
   }
   return totalCholate;
 }
+// 5. Given two strings, return true if they are anagrams of one another
+var isAnagram = function (s, t) {
+  s = s.toLowerCase();
+  t = t.toLowerCase();
+  if (s.length === t.length) {
+    for (let i = 0; i < s.length; i++) {
+      let isAvailable = t.includes(s[i]);
+      if (!isAvailable) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
