85373a239107a9ceffa5791f914500063ca75d8f
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-03-01 16:30:59 +0300

    add project

--- pagination.py
+++ pagination.py
@@ -52,3 +52,26 @@ def bubble_sort(arr):
 arr = [64, 34, 25, 12, 22, 11, 90]
 bubble_sort(arr)
 print("Sorted array:", arr)
+
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+arr = [2, 3, 4, 10, 40]
+target = 10
+result = binary_search(arr, target)
+if result != -1:
+    print("Element is present at index", result)
+else:
+    print("Element is not present in array")
