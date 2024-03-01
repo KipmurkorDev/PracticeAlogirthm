ba090896dab185bc71c6ce519097f04dc7935eac
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-03-01 16:29:58 +0300

    add project

--- pagination.py
+++ pagination.py
@@ -39,3 +39,16 @@ if result != -1:
     print("Element is present at index", result)
 else:
     print("Element is not present in array")
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr)
+print("Sorted array:", arr)
