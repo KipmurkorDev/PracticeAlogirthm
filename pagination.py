6f86f04044ad56abffdaf957f54de8808bf9c4ce
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-03-01 16:29:37 +0300

    add project

--- pagination.py
+++ pagination.py
@@ -10,9 +10,32 @@ class Server:
         """Cached dataset
         """
         if self.__dataset is None:
-           pass
+            pass
 
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10):
-            pass
+        pass
+
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+arr = [2, 3, 4, 10, 40]
+target = 10
+result = binary_search(arr, target)
+if result != -1:
+    print("Element is present at index", result)
+else:
+    print("Element is not present in array")
