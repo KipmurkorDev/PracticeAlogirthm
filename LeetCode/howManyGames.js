912b6ade6d693943b4dad4b7c9c4989c1ba768cf
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-03-11 19:31:52 +0300

    add new

--- LeetCode/howManyGames.js
+++ LeetCode/howManyGames.js
@@ -24,3 +24,25 @@ const countChar = (str, char) => {
   }
   return count + countChar(str.slice(0, -1), char);
 };
+// This is probably one of the less challenging challenges (no pun intended) — in terms of difficulty — but that doesn’t detract from the fact that you could come across it during a job interview. It goes like this.
+
+const findVowels = (str) => {
+  let count = 0;
+  let vowels = ["a", "e", "i", "o", "u"];
+  for (let i = 0; i < str.length; i++) {
+    let isVowel = vowels.includes(str[i]);
+    if (isVowel) {
+      count++;
+    }
+  }
+  return count;
+};
+// A palindrome is a word, phrase, or other type of string that can be read backwards or upside down. For example, “racecar” and “Anna” are palindromes. “Tisch” and “Juan” are not palindromes because they do not read the same from left to right and from right to left.
+const palindrome = (str) => {
+  str = str.toLowerCase();
+  let strReverse = str.split("").reverse().join("");
+  if (strReverse === str) {
+    return true;
+  }
+  return false;
+};
