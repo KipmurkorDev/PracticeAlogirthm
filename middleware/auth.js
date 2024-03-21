e2d604664831586d1cce7189fa0b8214066f4dd2
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-03-21 14:59:16 +0300

    add

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,51 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 module.exports = function (req, res, next) {
-  const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied. No token provided.');
+  const token = req.header("x-auth-token");
+  if (!token) return res.status(401).send("Access denied. No token provided.");
 
   try {
-    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-    req.user = decoded; 
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    req.user = decoded;
     next();
+  } catch (ex) {
+    res.status(400).send("Invalid token.");
   }
-  catch (ex) {
-    res.status(400).send('Invalid token.');
+};
+
+// A palindrome is a word, phrase, or other type of string that can be read backwards or upside down. For example, “racecar” and “Anna” are palindromes. “Tisch” and “Juan” are not palindromes because they do not read the same from left to right and from right to left.
+const palindrome = (str) => {
+  str = str.toLowerCase();
+  let strReverse = str.split("").reverse().join("");
+  if (strReverse === str) {
+    return true;
   }
-}
+  return false;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let arrToSet = new Set(nums);
+  let newArr = [...arrToSet];
+  return newArr;
+};
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let index = 0;
+  let set = new Set();
+
+  for (let num of nums) {
+    if (!set.has(num)) {
+      nums[index++] = num;
+      set.add(num);
+    }
+  }
+  return index;
+};
