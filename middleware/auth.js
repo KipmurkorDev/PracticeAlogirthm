3893c85e96cb7e8d5ca9cd17d216118dc407c8a5
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-03-29 19:36:45 +0300

    daa

--- middleware/auth.js
+++ middleware/auth.js
@@ -147,14 +147,7 @@ module.exports = function (handler) {
   };
 };
 // A palindrome is a word, phrase, or other type of string that can be read backwards or upside down. For example, “racecar” and “Anna” are palindromes. “Tisch” and “Juan” are not palindromes because they do not read the same from left to right and from right to left.
-const palindrome = (str) => {
-  str = str.toLowerCase();
-  let strReverse = str.split("").reverse().join("");
-  if (strReverse === str) {
-    return true;
-  }
-  return false;
-};
+
 function divisibleSumPairs(n, k, ar) {
   // Write your code here
   let count = 0;
