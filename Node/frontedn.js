3e2915ac694537886dfb03ca3ad4acf3fc6a3c91
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-05-17 09:30:47 +0300

    datt

--- Node/frontedn.js
+++ Node/frontedn.js
@@ -215,3 +215,20 @@ function library() {
   }
   classicLiterature();
 }
+
+// <-- 4. JavaScript engine looks here last
+const globalVar = "I am in the global scope";
+
+function outerOuterFunction() {
+  // <-- 3. JavaScript engine looks here third
+  const outerOuterVar = "I am in the outerOuterFunction scope";
+  function outerFunction() {
+    // <-- 2. JavaScript engine looks here second
+    const outerVar = "I am in the outerFunction scope";
+    function innerFunction() {
+      // <-- 1. JavaScript engine looks here first
+      const innerVar = "I am in the innerFunction scope";
+      console.log(globarVar);
+    }
+  }
+}
