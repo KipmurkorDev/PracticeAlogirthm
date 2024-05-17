9e7277edeec5eb7af3cafaf55ba3257d269ea0b0
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-05-17 09:28:59 +0300

    datt

--- Node/frontedn.js
+++ Node/frontedn.js
@@ -204,3 +204,14 @@ function sayHello() {
   const message = "Hello!";
   console.log(message);
 }
+const bookSeeker = "James";
+const book = "Great Expectations";
+function library() {
+  const librarian = "Julia";
+  console.log(bookSeeker + " asked " + librarian + " for " + book);
+  function classicLiterature() {
+    const shelf = "Dickens";
+    console.log(bookSeeker + " found " + book + " on the " + shelf + " shelf!");
+  }
+  classicLiterature();
+}
