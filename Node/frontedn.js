38d7b9dfa8714c26746e3371d87e55f600d45644
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-05-17 09:36:36 +0300

    datt

--- Node/frontedn.js
+++ Node/frontedn.js
@@ -253,3 +253,20 @@ if (fruit !== "banana") {
   console.log(fruit);
 }
 console.log(fruit);
+function whereAmIAssessible(a) {
+  if (a) {
+    var x = "x is accessible";
+    let y = "y is accessible";
+    const z = "z is accessible";
+    console.log("Inside the if block scope:");
+    console.log(x);
+    console.log(y);
+    console.log(z);
+  }
+}
+
+whereAmIAssessible(true);
+console.log("Outside the function scope:");
+console.log(x);
+console.log(y);
+console.log(z);
