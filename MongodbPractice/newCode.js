8f46899af74ad17a80f12371170350b821c74cb3
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-05-19 13:18:12 +0300

    datt

--- MongodbPractice/newCode.js
+++ MongodbPractice/newCode.js
@@ -5,3 +5,21 @@ const arraysInArrays = [
   ["Julia", "James"],
   [true, false, true, false],
 ];
+const donuts2 = [
+  "glazed", // index is 0
+  "chocolate frosted", // index is 1
+  "cinnamon", // index is 2
+  "sprinkled", // index is 3
+  "powdered", // index is 4
+  "cinnamon sugar", // index is 5
+  "glazed cruller", // index is 6
+  "chocolate cruller", // index is 7
+  "cookies", // index is 8
+  "Boston creme", // index is 9
+  "powdered jelly filled", // index is 10
+  "creme de leche", // index is 11
+  "glazed donut holes", // index is 12
+  "blueberry donut holes", // index is 13
+  "cake donut holes", // index is 14
+  "chocolate donut holes", // index is 15
+];
