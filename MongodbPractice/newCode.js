247f9ca73fcf6f1a0718ead53c61182d6100489a
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-05-19 13:15:23 +0300

    datt

--- MongodbPractice/newCode.js
+++ MongodbPractice/newCode.js
@@ -1,2 +1,7 @@
 const donuts = ["glazed", "powdered", "jelly"];
 const mixedData = ["abcd", 1, true, undefined, null, "all the things"];
+const arraysInArrays = [
+  [1, 2, 3],
+  ["Julia", "James"],
+  [true, false, true, false],
+];
