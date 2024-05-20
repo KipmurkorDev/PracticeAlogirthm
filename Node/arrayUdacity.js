a9af079a567de9b358fe4135f1dfff6a54093ebd
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-05-20 08:56:57 +0300

    dsa

--- Node/arrayUdacity.js
+++ Node/arrayUdacity.js
@@ -13,3 +13,12 @@ index = -2;
 
 console.log(`An index of ${index} returns ${array1.at(index)}`);
 // Expected output: "An index of -2 returns 130"
+const array1 = ['a', 'b', 'c', 'd', 'e'];
+
+// Copy to index 0 the element at index 3
+console.log(array1.copyWithin(0, 3, 4));
+// Expected output: Array ["d", "b", "c", "d", "e"]
+
+// Copy to index 1 all elements from index 3 to the end
+console.log(array1.copyWithin(1, 3));
+// Expected output: Array ["d", "d", "e", "d", "e"]
