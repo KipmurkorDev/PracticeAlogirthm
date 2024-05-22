d9c9dc39a5af1fe32eb1b63ebd817b5bdcebd297
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-05-22 09:32:05 +0300

    dsa

--- Node/settingOwnThis.js
+++ Node/settingOwnThis.js
@@ -12,3 +12,19 @@ function multiply(n1, n2) {
   return n1 * n2;
 }
 multiply.apply(window, [3, 4]);
+function Dalmatian(name) {
+  this.name = name;
+
+  this.bark = function () {
+    console.log(`${this.name} barks!`);
+  };
+}
+// (B)
+
+function Dalmatian(name) {
+  this.name = name;
+}
+
+Dalmatian.prototype.bark = function () {
+  console.log(`${this.name} barks!`);
+};
