fb893c4bbe4e977d58ed284392f0580218fd107a
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-05-22 08:25:37 +0300

    dsa

--- Node/object.js
+++ Node/object.js
@@ -5,3 +5,12 @@ function SoftwareDeveloper(name) {
   this.favoriteLanguage = "JavaScript";
   this.name = name;
 }
+function Finch(name) {
+  this.kingdom = "Animalia";
+  this.name = name;
+}
+
+function Sparrow(name) {
+  this.kingdom = "Animalia";
+  this.name = name;
+}
