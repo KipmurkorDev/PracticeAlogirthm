563c8ebbfac9705925712d6bfd033895b0117a64
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-05-23 07:26:48 +0300

    dsa

--- Node/settingOwnThis.js
+++ Node/settingOwnThis.js
@@ -19,12 +19,14 @@ function Dalmatian(name) {
     console.log(`${this.name} barks!`);
   };
 }
-// (B)
-
 function Dalmatian(name) {
   this.name = name;
 }
 
 Dalmatian.prototype.bark = function () {
   console.log(`${this.name} barks!`);
+};
+const bear = {
+  claws: true,
+  diet: "carnivore",
 };
