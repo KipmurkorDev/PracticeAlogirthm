dbb7cb1ec1045f9c2bf0bfbae572f4ba541803a5
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-05-23 08:02:37 +0300

    dsa

--- Node/settingOwnThis.js
+++ Node/settingOwnThis.js
@@ -56,3 +56,17 @@ let source = { number: 7 };
 Object.assign(target, source);
 
 console.log(target);
+const duck = {
+  hasBill: true,
+};
+const beaver = {
+  hasTail: true,
+};
+const otter = {
+  hasFur: true,
+  feet: "webbed",
+};
+
+const platypus = Object.assign({}, duck, beaver, otter);
+
+console.log(platypus);
