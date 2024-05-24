84474adc0f63c0dbbcdfe0645d69e11e1ba6aa2d
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2024-05-24 08:32:55 +0300

    datt

--- Node/objetDesign.js
+++ Node/objetDesign.js
@@ -11,3 +11,8 @@ const emmanuel = Object.create(person);
 // using assign for matiple in heritcanse
 
 const timon = Object.assign({}, person);
+
+// using object method
+
+const myprotype = Object.getPrototypeOf(emmanuel);
+const isPropert = person.hasOwnProperty(education);
